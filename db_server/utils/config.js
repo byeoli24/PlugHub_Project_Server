501d7b98ddab9b2f460795214367f448484123bb
MinJi-You <136569313+byeoli24@users.noreply.github.com>
2023-06-14 17:34:17 +0900

    Update config.js

--- db_server/utils/config.js
+++ db_server/utils/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   dbConfig: {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'plughub'
+    host: 
+    port: 
+    user: 
+    password:
+    database:
   }
 } 
